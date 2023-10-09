98b93043f7d01f1299ffe5e497511b522f6ee329
tawfik575 <tawfikbsmrstucse@gmail.com>
2023-10-09 23:40:18 +0600

    populate products from the database

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Product } from './product';
+import { AdminPanelService } from '../admin-panel/admin-panel.service';
 
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html',
   styleUrls: ['./products.component.css']
 })
-export class ProductsComponent {
+export class ProductsComponent implements OnInit {
+  products: Product[] = [];
+  constructor(private adminPanelService: AdminPanelService) { }
 
+  ngOnInit() {
+    this.adminPanelService.fetchProducts();
+    this.adminPanelService.getProducts().subscribe({
+      next: (data) => {
+        this.products = data;
+      }
+    });
+  }
 }
