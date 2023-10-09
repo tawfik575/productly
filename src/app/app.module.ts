0103fcc3045ee474f25a679800bf57d33f84b29c
tawfik575 <tawfikbsmrstucse@gmail.com>
2023-10-09 08:45:32 +0600

    add admin panel

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,23 @@ import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { ProductsComponent } from './products/products.component';
 import { FooterComponent } from './footer/footer.component';
+import { AdminPanelComponent } from './admin-panel/admin-panel.component';
+import { AdminNavComponent } from './admin-panel/admin-nav/admin-nav.component';
+import { ProductsListComponent } from './admin-panel/products-list/products-list.component';
+import { UsersComponent } from './admin-panel/users/users.component';
+import { AdminsComponent } from './admin-panel/admins/admins.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     ProductsComponent,
-    FooterComponent
+    FooterComponent,
+    AdminPanelComponent,
+    AdminNavComponent,
+    ProductsListComponent,
+    UsersComponent,
+    AdminsComponent
   ],
   imports: [
     BrowserModule,
